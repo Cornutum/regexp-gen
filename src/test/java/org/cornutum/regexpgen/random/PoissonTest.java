4ad048492eff0e575a6ac2747e1a0fdd2629e5f6
Kerry Kimbrough <kerrykimbrough@gmail.com>
2020-07-20 13:34:00 -0500

    PoissonTest: Parameterize with system properties

--- src/test/java/org/cornutum/regexpgen/random/PoissonTest.java
+++ src/test/java/org/cornutum/regexpgen/random/PoissonTest.java
@@ -7,6 +7,7 @@
 
 package org.cornutum.regexpgen.random;
 
+import org.apache.commons.lang3.StringUtils;
 import org.junit.Test;
 import static org.hamcrest.MatcherAssert.*;
 import static org.hamcrest.Matchers.*;
@@ -30,9 +31,9 @@ public class PoissonTest
   public void expectedLambda()
     {
     // Given...
-    int lambda = 4;
-    Poisson values = new Poisson( new Random(), lambda);
-    int count = 1000;
+    int lambda = lambda();
+    Poisson values = new Poisson( new Random( randomSeed_), lambda);
+    int count = getSampleCount();
     
     // When...
     List<Integer> generated =
@@ -44,14 +45,56 @@ public class PoissonTest
     double average = generated.stream().collect( averagingInt( Integer::intValue));
     assertThat( "Average", (int) Math.round( average), is( lambda));
 
-    int max = generated.stream().max( Integer::compareTo).orElse( 0);
-    Map<Integer,List<Integer>> counts = generated .stream().collect( groupingBy( Function.identity()));
-    IntStream.range( 0, max)
-      .forEach( i ->    {
-        System.out.println(
-          String.format( "%4s: %4s", i, Optional.ofNullable( counts.get(i)).map( List::size).orElse( 0))
-          );
-        });
+    if( printResults())
+      {
+      System.out.println( String.format( "Lambda=%s", lambda));
+      int max = generated.stream().max( Integer::compareTo).orElse( 0);
+      Map<Integer,List<Integer>> counts = generated .stream().collect( groupingBy( Function.identity()));
+      IntStream.range( 0, max)
+        .forEach( i ->    {
+          System.out.println(
+            String.format(
+              "%4s: %4s",
+              i,
+              Optional.ofNullable( counts.get(i)).map( List::size).orElse( 0)));
+          });
+      }
     }
 
+  /**
+   * Returns true if printing results.
+   */
+  private boolean printResults()
+    {
+    return Optional.ofNullable( System.getProperty( "printResults")).map( Boolean::valueOf).orElse( false);
+    }
+
+  /**
+   * Returns the lambda parameter for this test.
+   */
+  private int lambda()
+    {
+    return
+      Optional.ofNullable( StringUtils.trimToNull( System.getProperty( "lambda")))
+      .map( Integer::valueOf)
+      .orElse( 16);
+    }
+
+  /**
+   * Returns the number of samples for this test.
+   */
+  private int getSampleCount()
+    {
+    return Optional.ofNullable( System.getProperty( "count")).map( Integer::valueOf).orElse( 1000);
+    }
+
+  private static long randomSeed_;
+  static
+    {
+    randomSeed_ =
+      Optional.ofNullable( StringUtils.trimToNull( System.getProperty( "seed")))
+      .map( Long::valueOf)
+      .orElse( new Random().nextLong());
+    System.out.println( String.format( "PoissonTest: randomSeed=%s", randomSeed_));
+    }
   }
